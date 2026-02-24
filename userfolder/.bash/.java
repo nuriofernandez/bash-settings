f0b99604d77be0b6bd907e4e6a5c33b56682b904
Nurio Fernandez <nurio@Nurios-MacBook-Pro.local>
2026-02-24 10:59:45 +0100

    set openjdk 25 as path

--- userfolder/.bash/.java
+++ userfolder/.bash/.java
@@ -3,6 +3,7 @@ if [ "$(uname -s)" = "Darwin" ]; then
     #export PATH="/opt/homebrew/opt/openjdk@11/bin:$PATH"
     #export PATH="/opt/homebrew/opt/openjdk@17/bin:$PATH"
     #export PATH="/Library/Internet Plug-Ins/JavaAppletPlugin.plugin/Contents/Home/bin:$PATH"
-    export PATH="/opt/homebrew/opt/openjdk@21/bin:$PATH"
+    #export PATH="/opt/homebrew/opt/openjdk@21/bin:$PATH"
+    export JAVA_HOME=$(/usr/libexec/java_home -v 25)
 fi
 
