7f4f76926635c325551df86c3b126c0d84900603
Nurio Fernandez <nurio@Nurios-MacBook-Pro.local>
2026-02-24 10:56:13 +0100

    Update gopath path

--- userfolder/.bash/.go
+++ userfolder/.bash/.go
@@ -1,7 +1,7 @@
 # Only for mac
 if [ "$(uname -s)" = "Darwin" ]; then
-	export GOPATH=/Users/n.fernandez/go/gopath
-	export GOPRIVATE=gitlab.com/eslfaceitgroup/*
+	export GOPATH=/Users/nurio/go/gopath
+	# export GOPRIVATE=gitlab.com/eslfaceitgroup/*
 	export PATH=$PATH:$GOPATH/bin
 	export GOBIN=$(go env GOPATH)/bin
 fi
